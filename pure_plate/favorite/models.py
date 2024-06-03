31eda0047aaabf0a7cdf0490836837c98717d44d
ChanHHaeng <dlcksgod2325@gmail.com>
2024-06-03 18:45:39 +0900

    docs: add favorate annotation

--- pure_plate/favorite/models.py
+++ pure_plate/favorite/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-
-
 from account.models import User
 from restaurant.models import Restaurant
 
-
 class Favorite(models.Model):
+    """
+    Model to represent a user's favorite restaurant.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('user', 'restaurant')
-
+        unique_together = ('user', 'restaurant')  # Ensure each user-restaurant pair is unique
